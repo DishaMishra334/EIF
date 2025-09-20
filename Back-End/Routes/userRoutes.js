1ae543477084c2605ede09d4a38380564874b802
Disha <dishamishra335@gmail.com>
2025-09-20 12:37:30 +0530

    Update userRoutes.js

--- Back-End/Routes/userRoutes.js
+++ Back-End/Routes/userRoutes.js
@@ -1,7 +1,7 @@
 // backend/Routes/userRoutes.js
 
 import express from "express";
-import { registerUser, loginUser, getUserOrders } from "../controllers/userController.js";
+import { registerUser, loginUser, getUserOrders, adminlogin } from "../controllers/userController.js";
 import authMiddleware from "../middleware/auth.js";
 
 const userRouter = express.Router();
@@ -9,6 +9,6 @@ const userRouter = express.Router();
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 userRouter.get('/orders', authMiddleware, getUserOrders); // This route handles fetching user orders
-
+userRouter.post("/admin", adminlogin);
 export default userRouter;
  
